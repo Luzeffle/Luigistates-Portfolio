ab61ad159d3300f3161b5346ecfd305216d7bb76
Luzeffle <165398543+Luzeffle@users.noreply.github.com>
2026-03-17 23:11:19 +0800

    Changed metadata to show images when pasting url

--- operator-portfolio/app/layout.tsx
+++ operator-portfolio/app/layout.tsx
@@ -19,11 +19,32 @@ const playfair = Playfair_Display({
   variable: "--font-playfair",
 });
 
-export const metadata = {
-  title: "@Luigistates",
-  description: "Like fireflies to a flame... life begets death.",
-  icons: {
-    icon: "/profile.webp", // This changes the small icon (favicon) in the tab too!
+export const metadata: Metadata = {
+  title: "Luigistates | Portfolio",
+  description: "Don't forget to use my code!",
+  // This is the magic section for link previews!
+  openGraph: {
+    title: "Luigistates | Portfolio",
+    description: "Don't forget to use my code!",
+    url: "https://luigistates-portfolio.vercel.app/",
+    siteName: "Luigistates Portfolio",
+    images: [
+      {
+        url: "/opengraph-image.png", // Put this image in your 'public' folder
+        width: 1200,
+        height: 630,
+        alt: "Luigistates Portfolio Preview Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  // Twitter uses its own slightly different system
+  twitter: {
+    card: "summary_large_image",
+    title: "Luigistates | Portfolio",
+    description: "Don't forget to use my code!",
+    images: ["/opengraph-image.png"], // Same image as above
   },
 };
 
